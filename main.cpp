ad2889edffa5162c29a5ff7f6126812f904778ac
Masayuki Anekawa <anekawamasayuki@gmail.com>
2021-09-19 01:40:29 +0900

    baseline: こっからはじめよう

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 // comment to disable debug functions
 #define DEBUG
 
-
 // frequent used aliases
 typedef long long ll;
 typedef pair<int, int> p;
@@ -97,7 +96,10 @@ static const int dj[] = {0, 1, 0, -1};
 ///////// Main /////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////
 
-ll N,M,K,A[NMAX][NMAX],B[NMAX][NMAX],C[NMAX][NMAX],D,E;
+// timer func
+inline int get_time_millisec(void){
+    return static_cast<ll>(chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now().time_since_epoch()).count()) % INT_MAX;
+}
 
 struct Mountain {
   p pos;
@@ -107,13 +109,40 @@ struct Mountain {
 typedef std::vector<Mountain> ans_t;
 
 #define NUM_BRANCH 3
-ll Q = 100, NOM = 100;
+ll A[NMAX][NMAX],B[NMAX][NMAX];
+ll N = 10, Q = 10, NOM = 10, ansScore = LLONG_MAX;
+int threshold = 75; // 100分のいくつの確率で最小ロス近傍に遷移するか
 ans_t branch[NUM_BRANCH];
 ans_t ans;
 
-void greedy(){
-  // TODO
-  ans = {{{0, 0}, 100}};
+ans_t generate_random(){
+  ll tries = 10;
+  auto ret = vector<Mountain>(Q);
+  FOR(k, N) {
+    int i = rand() % N;
+    int j = rand() % N;
+    int h = rand() % N;
+    ret[k] = {{i, j}, h};
+  }
+
+  return ret;
+}
+
+void calc_B(ans_t ans_cand){
+  Fill(B, 0);
+  for (size_t i = 0; i < ans.size(); i++)
+  {
+    int y = ans[i].pos.fi;
+    int x = ans[i].pos.se;
+    int h = ans[i].h;
+    for (size_t j = max(0, y - h); j < min<int>(N, y + h); j++)
+    {
+      for (size_t k = max(0, x - h); k < min<int>(N, x + h); k++)
+      {
+        B[j][k] += max<int>(0, h - abs<int>(j - y) - abs<int>(k - x));
+      }
+    }
+  }
 }
 
 ll calc_diff(){
@@ -124,13 +153,22 @@ ll calc_diff(){
   return diff;
 }
 
+ll calcScore(ans_t ans_cand){
+  calc_B(ans_cand);
+  ll diff = calc_diff();
+  debug("diff: %lld\n", diff);
+  ll basePoints = 2e8 - diff;
+  // TODO: calc bonus
+  return basePoints;
+}
+
 void create_branch() {
   for (int i = 0; i < NUM_BRANCH; i++)
   {
     branch[i] = vector<Mountain>(Q);
     FOR(j,N) branch[i][j] = ans[j]; // とりあえずansをコピー
 
-    for(int j = 0; j < NOM; ++j)
+    for(int k = 0; k < NOM; ++k)
     {
       // 1こ選んで
       size_t chg_index = rand() % Q;
@@ -146,28 +184,34 @@ void create_branch() {
 }
 
 void select_branch() {
-  // TODO
-  ans = branch[0];
+  auto maxAns = ans;
+  ll maxScore = ansScore;
+  for (int i = 0; i < NUM_BRANCH; i++)
+  {
+    ll candScore = calcScore(branch[i]);
+    debug("candScore for branch[%d]: %lld\n", i, candScore);
+    if (candScore > maxScore) {
+      maxScore = candScore;
+      maxAns = branch[i];
+    }
+  }
+  if (rand() % 100 < threshold)
+  {
+    debug("Transitioned to new answer (Q: %d, score: %lld).\n",
+      maxAns.size(), maxScore);
+    ans = maxAns;
+    ansScore = maxScore;
+  }
+  else
+  {
+    debug("Stayed at current answer (Q: %d, score: %lld).\n",
+      ans.size(), ansScore);
+  }
 }
 
 void update_NOM() {
   // TODO
   --NOM;
-}
-
-ll calc_score(ans_t ans_cand){
-  ll basePoints = 2e8 - calc_diff();
-  // TODO: calc bonus
-  return basePoints;
-}
-
-void calc_B(ans_t ans_cand){
-  // TODO
-  // for (size_t i = 0; i < ans.size(); i++)
-  // {
-
-  // }
-  FOR(i,N) FOR(j,N) B[i][j] = rand() % AMAX;
 }
 
 void debug(){
@@ -179,17 +223,17 @@ void debug(){
     printf("i: %d, X: %d, Y: %d, H: %lld\n",
       i, ans[i].pos.fi, ans[i].pos.se, ans[i].h);
   }
+  cout << "B: " << endl;
+  FOR(i, N) {
+    FOR(j,N-1) printf("%3lld, ", B[i][j]);
+    printf("%3lld\n", B[i][N-1]);
+  }
 }
 
 void solve(){
-  // まずいっこ
-  NOM = 100;
-  greedy();
   // Simulate Annealing
-  // ll diff = calc_diff();
-  ll score = INT_MAX;
   int t = 0;
-  int MAX_TIME = 1;
+  int MAX_TIME = 5;
 
   while(t < MAX_TIME/* && diff > 0*/) {
     create_branch();
@@ -213,20 +257,49 @@ void answer(){
 
 void init(){
   // initialize for each test case
+  // TODO: なんかrand()がうまく動いてないみたいだから他の乱数器を検討して
+  std::srand(get_time_millisec()); //use current time as seed for random generator
+
   Fill(B, 0);
+  ans = generate_random();
+  ansScore = calcScore(ans);
+}
+
+void export_ans() {
+  std::ostringstream stringStream;
+  stringStream << "generated_n" << Q << "/" << rand() % 10000 << ".txt";
+  std::string filename = stringStream.str();
+
+  std::ofstream file;
+  file.open(filename, std::ios::out);
+  FOR(i,N) {
+    FOR(j, N-1) file << B[i][j] << " ";
+    file << B[i][N-1] << endl;
+  }
+  file.close();
+  printf("Exported to %s.\n", filename.c_str());
 }
 
 int main(int argc, char const *argv[])
 {
   init();
-  N = 100;
+
+  if (argc > 1 && strcmp(argv[1], "--generate") == 0) {
+    printf("GENERATE MODE: exporting randomly genarated board...\n");
+    export_ans();
+    return 0;
+  }
+
   for(auto i = 0; i < N; ++i)
     for(auto j = 0; j < N; ++j)
       cin >> A[i][j];
+
   solve();
+
   #ifdef DEBUG
   debug();
   #endif
+
   answer();
 
   return 0;
